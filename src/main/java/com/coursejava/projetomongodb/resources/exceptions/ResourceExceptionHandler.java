3f3049075364db1b62f2d91594571c4317cc4149
lucasfernande <lucas.fernandes6192@outlook.com>
2020-05-20 13:02:40 -0300

    Obtendo usuário por id e tratamento de exceção

---
+++ src/main/java/com/coursejava/projetomongodb/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.coursejava.projetomongodb.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.coursejava.projetomongodb.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		String error = "Objeto não encontrado";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
--- src/main/java/com/coursejava/projetomongodb/services/UserService.java
+++ src/main/java/com/coursejava/projetomongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.coursejava.projetomongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.coursejava.projetomongodb.domain.User;
 import com.coursejava.projetomongodb.repositories.UserRepository;
+import com.coursejava.projetomongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
