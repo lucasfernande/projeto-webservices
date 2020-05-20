418bab404f16d83ffd8fbd070bfabd6eb0c60985
lucasfernande <lucas.fernandes6192@outlook.com>
2020-05-20 13:14:44 -0300

    Inserção de usuário com Post

--- src/main/java/com/coursejava/projetomongodb/services/UserService.java
+++ src/main/java/com/coursejava/projetomongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.coursejava.projetomongodb.domain.User;
+import com.coursejava.projetomongodb.dto.UserDTO;
 import com.coursejava.projetomongodb.repositories.UserRepository;
 import com.coursejava.projetomongodb.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO dto) {
+		return new User(dto.getId(), dto.getName(), dto.getEmail());
 	}
 }
